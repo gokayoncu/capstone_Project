efa3c3607c811094638327931ea5a2dd720ecd3d
gokayoncu <gokay61__61@hotmail.com>
2024-06-09 21:01:16 +0300

    Project End

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,47 +1,91 @@
 import React from 'react'
-import Slider from "react-slick";
-import './home.css'
 import { NavLink } from 'react-router-dom';
+import './home.css'
 
 function Home() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    adaptiveHeight:true,
-    arrows:true,
-    autoplay:true,
-    autoplaySpeed: 1500
-  };
+  
   return (    
-  <div>
-    <Slider {...settings} className='carousel'>
-    <div className='carousel-box'>
-      <img src="https://worldanimalfoundation.org/wp-content/uploads/2023/09/Cute-dogs.jpg" alt="" className='carousel-img'/>
-      <div className='image-text'>
-        <h2>Vet App Veteriner Kliğini</h2>
-        <span>Sevimli dostlarımız için profesyonel hizmet</span>
-      </div>
-      <div className='opacity'></div>
-    </div>
-    <div className='carousel-box'>
-      <img src="https://www.vetorium.com/upload/haberler/34/GaleriImg/operasyon-oncesi-bilgiler.jpg" alt="" className='carousel-img'/>
-      <div className='image-text'>
-        <h2>Güler Yüzlü Ekiplerimiz</h2>
-      </div>
-      <div className='opacity'></div>
-    </div>
-    <div className='carousel-box'>
-      <img src="https://www.kurtkoymonavet.com/wp-content/uploads/2024/01/kurtkoy-veteriner-dogum-jinekoloji.webp" alt="" className='carousel-img'/>
-      <div className='image-text'>
-        <h2>Alanında Uzman Veterinerler</h2>
-      </div>
-      <div className='opacity'></div>
-    </div>
-    
-  </Slider>
+  <div className='wrapper'>
+    <div className='w-container'>
+        <input type="radio" name='slide' id='c1' defaultChecked/>
+        <label htmlFor="c1" className='card'>
+            <div className='row'>
+              <div className="icon">
+                1
+              </div>
+              <div className="description">
+                <NavLink to='/customer' ><span >Müşteri Yönetimi</span></NavLink>
+                <span style={{paddingBottom:0}}>Yeni müşterilerin bilgilerini eklenmesi. Mevcut müşterilerin listelenmesi ve bilgilerini güncellenebileceğiniz sisteme ulaşmak için</span>
+                <span style={{color:'red',paddingTop:0}}>Lütfen Başlığa Tıklayınız</span>
+              </div>      
+            </div>
+        </label>
+        <input type="radio" name='slide' id='c2' defaultChecked/>
+        <label htmlFor="c2" className='card'>
+            <div className='row'>
+              <div className="icon">
+                2
+              </div>
+              <div className="description">
+                <NavLink to='/doctor' ><span >Doktor Yönetimi</span></NavLink>
+                <span style={{paddingBottom:0}}>Yeni Doktor bilgilerini eklenmesi. Mevcut Doktor listelenmesi ve bilgilerini güncellenebileceğiniz sisteme ulaşmak için</span>
+                <span style={{color:'red',paddingTop:0}}>Lütfen Başlığa Tıklayınız</span>
+              </div>      
+            </div>
+        </label>
+        <input type="radio" name='slide' id='c3' defaultChecked/>
+        <label htmlFor="c3" className='card'>
+            <div className='row'>
+              <div className="icon">
+                3
+              </div>
+              <div className="description">
+                <NavLink to='/animals' ><span >Hayvan Yönetimi</span></NavLink>
+                <span style={{paddingBottom:0}}>Yeni Hayvan bilgilerini eklenmesi. Mevcut Hayvan listelenmesi ve bilgilerini güncellenebileceğiniz sisteme ulaşmak için</span>
+                <span style={{color:'red',paddingTop:0}}>Lütfen Başlığa Tıklayınız</span>
+              </div>      
+            </div>
+        </label>
+        <input type="radio" name='slide' id='c4' defaultChecked/>
+        <label htmlFor="c4" className='card'>
+            <div className='row'>
+              <div className="icon">
+                4
+              </div>
+              <div className="description">
+                <NavLink to='/appointment' ><span >Randevu Yönetimi</span></NavLink>
+                <span style={{paddingBottom:0}}>Yeni Randevu bilgilerini eklenmesi. Mevcut Randevu listelenmesi ve bilgilerini güncellenebileceğiniz sisteme ulaşmak için</span>
+                <span style={{color:'red',paddingTop:0}}>Lütfen Başlığa Tıklayınız</span>
+              </div>      
+            </div>
+        </label>
+        <input type="radio" name='slide' id='c5' defaultChecked/>
+        <label htmlFor="c5" className='card'>
+            <div className='row'>
+              <div className="icon">
+                5
+              </div>
+              <div className="description">
+                <NavLink to='/report' ><span >Rapor Yönetimi</span></NavLink>
+                <span style={{paddingBottom:0}}>Yeni Rapor bilgilerini eklenmesi. Mevcut Rapor listelenmesi ve bilgilerini güncellenebileceğiniz sisteme ulaşmak için</span>
+                <span style={{color:'red',paddingTop:0}}>Lütfen Başlığa Tıklayınız</span>
+              </div>      
+            </div>
+        </label>
+        <input type="radio" name='slide' id='c6' defaultChecked/>
+        <label htmlFor="c6" className='card'>
+            <div className='row'>
+              <div className="icon">
+                6
+              </div>
+              <div className="description">
+                <NavLink to='/vaccination' ><span >Aşı Yönetimi</span></NavLink>
+                <span style={{paddingBottom:0}}>Yeni Aşı bilgilerini eklenmesi. Mevcut Aşı listelenmesi ve bilgilerini güncellenebileceğiniz sisteme ulaşmak için</span>
+                <span style={{color:'red',paddingTop:0}}>Lütfen Başlığa Tıklayınız</span>
+              </div>      
+            </div>
+        </label>
+    </div>    
   </div>
     
   )
